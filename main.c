6295bfb88b0f7206b82e78b5a257e44715c75e66
tr26236es <matheus.cavalcante@trf1.jus.br>
2023-11-28 16:24:14 -0300

    calculadora funfando 90%

--- main.c
+++ main.c
@@ -3,100 +3,54 @@
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
+#include "pilha.h"
 
-// Estrutura da pilha
-typedef struct No {
-    char *dado;
-    struct No *proximo;
-} No;
 
-typedef struct {
-    No *topo;
-} Pilha;
 
-// Inicializa a pilha
-void inicializar(Pilha *pilha) {
-    pilha->topo = NULL;
-}
-
-// Verifica se a pilha está vazia
-int pilhaVazia(Pilha *pilha) {
-    return pilha->topo == NULL;
-}
-
-// Empilha um valor
-void empilhar(Pilha *pilha, char *valor) {
-    No *novoNo = (No *)malloc(sizeof(No));
-    novoNo->dado = strdup(valor);
-    novoNo->proximo = pilha->topo;
-    pilha->topo = novoNo;
-}
-
-// Desempilha um valor
-char* desempilhar(Pilha *pilha) {
-    if (pilha->topo == NULL) {
-        return NULL;
-    }
-    No *temp = pilha->topo;
-    char *valor = temp->dado;
-    pilha->topo = temp->proximo;
-    free(temp);
-    return valor;
-}
-
-// Avalia a expressão pós-fixa
 double avaliarExpressaoPosfixada(char *expressao) {
     Pilha pilha;
     inicializar(&pilha);
 
     char *token = strtok(expressao, " ");
     while (token != NULL) {
-        if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1]))) {
-            empilhar(&pilha, token);
+        if (isdigit(token[0])) {
+            empilhar(&pilha, atof(token));
         } else {
-            double operando2 = atof(desempilhar(&pilha));
-            double operando1 = atof(desempilhar(&pilha));
-            double resultado;
+            double operando1 = 0.0, operando2 = 0.0, resultado = 0.0;
 
-            switch (token[0]) {
-                case '+': resultado = operando1 + operando2; break;
-                case '-': resultado = operando1 - operando2; break;
-                case '*': resultado = operando1 * operando2; break;
-                case '/': resultado = operando1 / operando2; break;
+            // Verificar se é uma operação unária
+            if (strcmp(token, "log") == 0 || strcmp(token, "sin") == 0 ||
+                strcmp(token, "cos") == 0 || strcmp(token, "tan") == 0) {
+                operando1 = desempilhar(&pilha);
+
+                if (strcmp(token, "sin") == 0) {
+                    resultado = sin(operando1);
+                } else if (strcmp(token, "cos") == 0) {
+                    resultado = cos(operando1);
+                } else if (strcmp(token, "tan") == 0) {
+                    resultado = tan(operando1);
+                } else if (strcmp(token, "log") == 0) {
+                    resultado = log10(operando1);
+                }
+            } else {
+                // Para operações binárias
+                operando2 = desempilhar(&pilha);
+                operando1 = desempilhar(&pilha);
+
+                if (strcmp(token, "+") == 0) {
+                    resultado = operando1 + operando2;
+                } else if (strcmp(token, "-") == 0) {
+                    resultado = operando1 - operando2;
+                } else if (strcmp(token, "*") == 0) {
+                    resultado = operando1 * operando2;
+                } else if (strcmp(token, "/") == 0) {
+                    resultado = operando1 / operando2;
+                } else if (strcmp(token, "^") == 0) {
+                    resultado = pow(operando1, operando2);
+                }
             }
 
-            char *resStr = (char *)malloc(20);
-            sprintf(resStr, "%f", resultado);
-            empilhar(&pilha, resStr);
-            free(resStr);
-        }
-        token = strtok(NULL, " ");
-    }
-
-    return atof(desempilhar(&pilha));
-}
-
-// Converte expressão pós-fixa para pré-fixa
-char* converterPosFixaParaPreFixa(char *expressao) {
-    Pilha pilha;
-    inicializar(&pilha);
-
-    char *token = strtok(expressao, " ");
-    while (token != NULL) {
-        if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1]))) {
-            empilhar(&pilha, token);
-        } else {
-            char *operando2 = desempilhar(&pilha);
-            char *operando1 = desempilhar(&pilha);
-
-            int size = strlen(operando1) + strlen(operando2) + 6;
-            char *novaExpressao = (char *)malloc(size);
-            snprintf(novaExpressao, size, "(%s %c %s)", operando1, token[0], operando2);
-
-            free(operando1);
-            free(operando2);
-
-            empilhar(&pilha, novaExpressao);
+            empilhar(&pilha, resultado);
         }
         token = strtok(NULL, " ");
     }
@@ -105,14 +59,9 @@ char* converterPosFixaParaPreFixa(char *expressao) {
 }
 
 int main() {
-    char expressao[] = "12 4 + 5 *"; // Exemplo de expressão pós-fixa
-    char *expressaoPreFixa = converterPosFixaParaPreFixa(expressao);
-    printf("Expressão Pré-fixa: %s\n", expressaoPreFixa);
-
-    char expressaoParaCalcular[] = "12 4 + 5 *"; // Exemplo de expressão pós-fixa para calcular
-    double resultado = avaliarExpressaoPosfixada(expressaoParaCalcular);
+    char expressao[] = "3 4 + 5 *";  // Exemplo de expressão pós-fixa
+    double resultado = avaliarExpressaoPosfixada(expressao);
     printf("Resultado: %f\n", resultado);
 
-    free(expressaoPreFixa);
     return 0;
 }
---
+++ pilha.c
@@ -0,0 +1,26 @@
+#include "pilha.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void inicializar(Pilha *pilha) {
+    pilha->topo = NULL;
+}
+
+void empilhar(Pilha *pilha, double valor) {
+    No *novoNo = (No *)malloc(sizeof(No));
+    novoNo->dado = valor;
+    novoNo->proximo = pilha->topo;
+    pilha->topo = novoNo;
+}
+
+double desempilhar(Pilha *pilha) {
+    if (pilha->topo == NULL) {
+        printf("Erro: Pilha vazia\n");
+        exit(EXIT_FAILURE);
+    }
+    No *temp = pilha->topo;
+    double valor = temp->dado;
+    pilha->topo = temp->proximo;
+    free(temp);
+    return valor;
+}
